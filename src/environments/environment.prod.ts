6ea5ef5d2c9c28c5a3581d2556708f6da7697309
Saswata95 <bunty.saswata@gmail.com>
2018-07-24 23:24:15 +0530

    Deploy commit 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconf: {
+    apiKey: "AIzaSyDsb61Fp2_Owov4v-iDiR29SxYnVEDV24g",
+    authDomain: "tekmeda-5ba8c.firebaseapp.com",
+    databaseURL: "https://tekmeda-5ba8c.firebaseio.com",
+    projectId: "tekmeda-5ba8c",
+    storageBucket: "",
+    messagingSenderId: "71586936152"
+  }
 };
